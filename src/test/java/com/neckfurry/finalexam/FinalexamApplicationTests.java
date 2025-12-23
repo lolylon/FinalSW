e8681f3ee6b9582778018b2a571d8f45bf0f4e6e
lolylon <neckfurry@gmail.com>
2025-12-23 16:47:56 +0500

    Basic structure

---
+++ src/test/java/com/neckfurry/finalexam/FinalexamApplicationTests.java
@@ -0,0 +1,15 @@
+package com.neckfurry.finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class FinalexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
